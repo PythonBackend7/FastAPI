27bc4a18e9c968347a4090c0e4cb08a88a99a001
alimardon <aboqijonov12@gmail.com>
2024-08-23 18:46:55 +0500

    fourth commit

---
+++ 04_dars/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    name = Column(String)
+    price = Column(Float)
+    description = Column(String)
+    image = Column(String or None)
